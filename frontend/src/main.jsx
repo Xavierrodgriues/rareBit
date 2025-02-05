002753a78391f8ce03d9d638557593763209b363
Xavierrodgriues <xavierrodgriues123@gmail.com>
2025-02-05 23:29:12 +0530

    Main Page UI done Partially

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from "./components/ui/provider"
 import './index.css'
 import App from './App.jsx'
+import {BrowserRouter} from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
